e2843b7ae234e837f3b95f700282411ee33a96af
thallium <thallium@users.noreply.github.com>
2022-07-16 03:42:34 +0000

    deploy: 3e302cf56c4fd0aa158dd62e9093ebe4888ee240

--- rust_cp/graph/sidebar-items.js
+++ rust_cp/graph/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["binary_lifting",""],["bipartite_matching",""],["shortest_path",""]]});
+initSidebarItems({"mod":[["binary_lifting",""],["bipartite_matching",""],["shortest_path",""],["tarjan",""]]});
---
+++ rust_cp/graph/tarjan/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["scc","Find strongly connected components of graph g. Components are numbered in reverse topological order, starting from 0. It returns the number of components and an array which indicates whichcomponent component each vertex belongs to"]]});
