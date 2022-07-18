e9c6a850ad50c10e75e443b2922446c40160240f
Gengchen <tuogengchen@gmail.com>
2022-07-18 09:20:36 -0400

    remove a test file

--- src/data_structure/mod.rs
+++ src/data_structure/mod.rs
@@ -1,6 +1,5 @@
 pub mod union_find;
 pub mod fenwick_tree;
-pub mod test;
 pub mod min_heap;
 pub mod sparse_table;
 pub mod lazy_segtree;
--- src/data_structure/test.rs
+++
@@ -1,5 +0,0 @@
-use crate::data_structure::fenwick_tree::FenwickTree;
-
-pub fn make_fen() -> FenwickTree<i32> {
-    FenwickTree::new(10)
-}
